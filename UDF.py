92f5176250ab7a296534b9ae0991a2e63fa3f32e
aslam208 <48829440+aslam208@users.noreply.github.com>
2023-02-03 22:38:06 +0400

    Update UDF.py

--- UDF.py
+++ UDF.py
@@ -6,3 +6,18 @@ for line in sys.stdin:
     skill=skill.upper()
     result = '\t'.join([str(int_val*10),skill])
     print(result)
+
+    #Select query 
+    
+    select transform(id, skill) using 'python3 transform_udf.py' as (new_id  int, new_skill string) from tbl_skill;
+    
+    # select query for single column
+    
+     select transform(id) using 'python3 multipy_udf.py' as new_id from tbl_skill;
+    
+import sys
+for line in sys.stdin:
+    line =line.strip("\n\r")
+    int_val=int(line)
+    result = int_val*10
+    print(str(result))
